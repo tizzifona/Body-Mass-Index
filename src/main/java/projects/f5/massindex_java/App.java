ea2dc7b5a870fb585ea6fc75fff1347a4887329e
tizzifona <tizzifona@gmail.com>
2024-11-18 09:51:00 +0100

    Added App.java

--- src/main/java/projects/f5/massindex_java/App.java
+++ src/main/java/projects/f5/massindex_java/App.java
@@ -1,17 +1,15 @@
 package projects.f5.massindex_java;
 
-/**
- * Hello world!
- */
-public final class App {
-    private App() {
-    }
+import projects.f5.massindex_java.controller.BmiController;
+import projects.f5.massindex_java.models.BmiModel;
+import projects.f5.massindex_java.models.Person;
+import projects.f5.massindex_java.view.BmiView;
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
+public final class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        BmiView view = new BmiView();
+        BmiModel model = new BmiModel(new Person(0, 0));
+        BmiController controller = new BmiController(view, model);
+        controller.run();
     }
 }
