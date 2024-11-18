cdea99a94a27fe9eb857eb4f4e4e474346e0afbb
tizzifona <tizzifona@gmail.com>
2024-11-18 09:21:46 +0100

    Added BmiView

--- src/main/java/projects/f5/massindex_java/view/BmiView.java
+++ src/main/java/projects/f5/massindex_java/view/BmiView.java
@@ -1,5 +1,24 @@
 package projects.f5.massindex_java.view;
 
+import java.util.Scanner;
+
 public class BmiView {
 
+    private Scanner scanner = new Scanner(System.in);
+
+    public double askWeight() {
+        System.out.println("Enter your weight in kilograms ");
+        return scanner.nextDouble();
+    }
+
+    public double askHeight() {
+        System.out.println("----------------------------");
+        System.out.println("Enter your height in meters ");
+        return scanner.nextDouble();
+    }
+
+    public void showResults(double bmi, String clasification) {
+        System.out.printf("Your BMI is: %.2f\n\n", bmi);
+        System.out.println("Your BMI is: " + clasification);
+    }
 }
