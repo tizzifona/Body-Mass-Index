e08767de500758433091634e909bb0e42f56cc91
tizzifona <tizzifona@gmail.com>
2024-11-18 09:17:18 +0100

    Added Person Model

--- src/main/java/projects/f5/massindex_java/models/Person.java
+++ src/main/java/projects/f5/massindex_java/models/Person.java
@@ -2,4 +2,27 @@ package projects.f5.massindex_java.models;
 
 public class Person {
 
+    private double weight;
+    private double height;
+
+    public Person(double weight, double height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
 }
